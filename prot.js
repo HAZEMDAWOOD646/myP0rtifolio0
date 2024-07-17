2498b80a7bc05e170d0a774fef882daab521aa6b
Hazem646 <139659586+HAZEMDAWOOD646@users.noreply.github.com>
2024-07-17 16:18:44 +0300

    Add files via upload

---
+++ prot.js
@@ -0,0 +1,5 @@
+function togglemenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle.toggle("open");
+    icon.classList.toggle.toggle("open");}
